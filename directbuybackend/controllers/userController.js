4066e0ab074b0bcc84e97e006465e05ced8bb54d
Iamneo <techadmin@iamneo.ai>
2023-12-05 08:11:02 +0000

    Auto-save-commit-12/5/2023--1:41:01-PM

--- directbuybackend/controllers/userController.js
+++ directbuybackend/controllers/userController.js
@@ -1,3 +1,4 @@
+const { generateToken } = require('../authUtils');
 const User = require('../models/userModel');
 
 
@@ -5,16 +6,19 @@ const User = require('../models/userModel');
 const getUserByUsernameAndPassword = async (req, res) => {
   try {
     const { email, password } = req.body;
-     console.log(" req.body", req.body);
+    console.log(" req.body", req.body);
     // Find a user with the given username and password
     const user = await User.findOne({ email, password });
-
+    const token = generateToken(user._id)
+    console.log("token",token);
+    user["token"] = token
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
 
     res.status(200).json(user);
   } catch (error) {
+
     res.status(500).json({ message: error.message });
   }
 };
@@ -22,10 +26,10 @@ const getUserByUsernameAndPassword = async (req, res) => {
 
 const addUser = async (req, res) => {
   try {
-    
-    console.log("req.body in signup",req.body);
+
+    console.log("req.body in signup", req.body);
     const user = await User.create(req.body);
-    res.status(200).json({"message":"Success"});
+    res.status(200).json({ "message": "Success" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- directbuybackend/routers/productRouter.js
+++ directbuybackend/routers/productRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const productController = require("../controllers/productController");
+const { validateToken } = require("../authUtils");
 const router = express.Router();
 
-router.post("/getAllProducts", productController.getAllProducts);
+router.post("/getAllProducts", validateToken,productController.getAllProducts);
 router.post("/getLoanApplicationById", productController.getProductByUserId);
 router.get("/getProductById/:id", productController.getProductById);
 router.post("/addProduct", productController.addProduct);
